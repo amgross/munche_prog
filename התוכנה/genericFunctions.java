f0140751ee94e6e2365aab0e883138e2038c9628
amgross <33063505+amgross@users.noreply.github.com>
2017-11-27 00:23:24 +0200

    fix bug with files that not ends with \n

--- התוכנה/genericFunctions.java
+++ התוכנה/genericFunctions.java
@@ -8,7 +8,7 @@ public class genericFunctions {
 	 * from https://stackoverflow.com/questions/453018/number-of-lines-in-a-file-in-java 
 	 * Gets path of a file.  
 	 * Return the number of lines in it.
-	 * 
+	 * we changed the origin code that will work even when their is no enter in the end of the file
 	 * @param filename
 	 * @return 1 or number of lines
 	 * @throws IOException
@@ -17,12 +17,17 @@ public class genericFunctions {
 		InputStream is = new BufferedInputStream(new FileInputStream(filename));
 		try {                 // try
 			byte[] c = new byte[1024];
-			int count = 0;
+			int count = 1;
 			int readChars = 0;
 			boolean empty = true;
+			byte last='1';
 			while ((readChars = is.read(c)) != -1) {
+				if(last=='\n'){
+					++count;
+				}
+				last=c[readChars-1];
 				empty = false;
-				for (int i = 0; i < readChars; ++i) {
+				for (int i = 0; i < readChars-1; ++i) {
 					if (c[i] == '\n') {
 						++count;
 					}
